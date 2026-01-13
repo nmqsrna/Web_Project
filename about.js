374cbc13bf3889be2119496d55ed13dc56ebad55
nmqsrna <92994547+nmqsrna@users.noreply.github.com>
2026-01-13 10:50:44 +0800

    13/01

--- about.js
+++ about.js
@@ -14,3 +14,23 @@ dropdownToggle.addEventListener('click', function(e) {
     dropdown.classList.remove('active');
     }
     });
+
+//button up 
+const backToTopBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.opacity = "1";
+    backToTopBtn.style.pointerEvents = "auto";
+  } else {
+    backToTopBtn.style.opacity = "0";
+    backToTopBtn.style.pointerEvents = "none";
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
--- index.js
+++ index.js
@@ -78,3 +78,24 @@ function resetAutoSlide() {
     clearInterval(autoSlide);
     startAutoSlide();
 }
+
+//button up 
+const backToTopBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.opacity = "1";
+    backToTopBtn.style.pointerEvents = "auto";
+  } else {
+    backToTopBtn.style.opacity = "0";
+    backToTopBtn.style.pointerEvents = "none";
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
